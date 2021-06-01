bbcdf3375eef89b4863fedf9803bb0eff263c967
stepochek <65595244+stepaniya@users.noreply.github.com>
2021-06-01 16:11:00 +0300

    firsttime

--- clever.py
+++ clever.py
@@ -32,64 +32,70 @@ def callback(recognizer, audio):
     try:
         voice = recognizer.recognize_google(audio, language="ru-RU").lower()
         print("[log] Распознано: " + voice)
+        speak_engine.say(voice)
+        speak_engine.runAndWait()
+        speak_engine.stop()
 
-        if voice.startswith(opts["alias"]):
-            # обращаются к Кеше
-            cmd = voice
-
-            for x in opts['alias']:
-                cmd = cmd.replace(x, "").strip()
-
-            for x in opts['tbr']:
-                cmd = cmd.replace(x, "").strip()
-
-            # распознаем и выполняем команду
-            cmd = recognize_cmd(cmd)
-            execute_cmd(cmd['cmd'])
+        # if voice.startswith(opts["alias"]):
+        #     # обращаются к Кеше
+        #     cmd = voice
+        #
+        #     for x in opts['alias']:
+        #         cmd = cmd.replace(x, "").strip()
+        #
+        #     for x in opts['tbr']:
+        #         cmd = cmd.replace(x, "").strip()
+        #
+        #     # распознаем и выполняем команду
+        #     cmd = recognize_cmd(cmd)
+        #     execute_cmd(cmd['cmd'])
 
     except sr.UnknownValueError:
         print("[log] Голос не распознан!")
+        speak_engine.say('Голос не распознан')
+        speak_engine.runAndWait()
+        speak_engine.stop()
     except sr.RequestError as e:
         print("[log] Неизвестная ошибка, проверьте интернет!")
 
 
-def recognize_cmd(cmd):
-    RC = {'cmd': '', 'percent': 0}
-    for c, v in opts['cmds'].items():
+# def recognize_cmd(cmd):
+#     RC = {'cmd': '', 'percent': 0}
+#     for c, v in opts['cmds'].items():
+#
+#         for x in v:
+#             vrt = fuzz.ratio(cmd, x)
+#             if vrt > RC['percent']:
+#                 RC['cmd'] = c
+#                 RC['percent'] = vrt
+#
+#     return RC
 
-        for x in v:
-            vrt = fuzz.ratio(cmd, x)
-            if vrt > RC['percent']:
-                RC['cmd'] = c
-                RC['percent'] = vrt
 
-    return RC
-
-
-def execute_cmd(cmd):
-    if cmd == 'ctime':
-        # сказать текущее время
-        now = datetime.datetime.now()
-        speak("Сейчас " + str(now.hour) + ":" + str(now.minute))
-
-    elif cmd == 'radio':
-        # воспроизвести радио
-        os.system("D:\\Jarvis\\res\\radio_record.m3u")
-
-    elif cmd == 'stupid1':
-        # рассказать анекдот
-        speak("Мой разработчик не научил меня анекдотам ... Ха ха ха")
-
-    else:
-        print('Команда не распознана, повторите!')
+# def execute_cmd(cmd):
+#     if cmd == 'ctime':
+#         # сказать текущее время
+#         now = datetime.datetime.now()
+#         speak("Сейчас " + str(now.hour) + ":" + str(now.minute))
+#
+#     elif cmd == 'radio':
+#         # воспроизвести радио
+#         os.system("D:\\Jarvis\\res\\radio_record.m3u")
+#
+#     elif cmd == 'stupid1':
+#         # рассказать анекдот
+#         speak("Мой разработчик не научил меня анекдотам ... Ха ха ха")
+#
+#     else:
+#         print('Команда не распознана, повторите!')
 
 
 # запуск
 r = sr.Recognizer()
 m = sr.Microphone(device_index=1)
 
-with m as source:
-    r.adjust_for_ambient_noise(source)
+# with m as source:
+#     r.adjust_for_ambient_noise(source)
 
 speak_engine = pyttsx3.init()
 
@@ -101,5 +107,5 @@ speak_engine = pyttsx3.init()
 speak("Добрый день, повелитель")
 speak("Кеша слушает")
 
-# stop_listening = r.listen_in_background(m, callback)
-# while True: time.sleep(0.1) # infinity loop
+stop_listening = r.listen_in_background(m, callback)
+while True: time.sleep(0.1) # infinity loop
